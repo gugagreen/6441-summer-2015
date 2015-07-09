9bf254fceba2153a769315ed80cfe26096a23dc1
Gallindo, Gustavo <gustavo.gallindo@sap.com>
2015-07-09 11:52:29 -0400

    #2 - setup service

--- lanterns/src/main/java/ca/concordia/lanterns/entities/Game.java
+++ lanterns/src/main/java/ca/concordia/lanterns/entities/Game.java
@@ -7,19 +7,32 @@ import java.util.Stack;
 import ca.concordia.lanterns.entities.enums.Colour;
 import ca.concordia.lanterns.entities.enums.DedicationType;
 
+/**
+ * Entity that contains all game state.
+ */
 public class Game {
 
+	/** Players in the game. */
 	private final Set<Player> players;
+	/** Lake where tiles are being displayed for all users. */
 	private final Lake lake;
+	/** Stack of Lantern Cards to be distributed. */
 	private final Stack<LanternCard>[] cards;
+	/** Stack of Dedication Tokens to be distributed. */
 	private final Stack<DedicationToken>[] dedications;
+	/** Quantity of favors to be distributed to players. */
 	private int favors;
 	
+	/**
+	 * Instantiates a new Game based on the player names.<br/>
+	 * This constructor will instantiate (but not populate) all attributes of this Game instance.
+	 * 
+	 * @param playerNames
+	 */
 	@SuppressWarnings("unchecked")
 	public Game(final Set<String> playerNames) {
 		super();
 		
-		// TODO - validate there are only 2-4 players
 		this.players = new HashSet<Player>();
 		for (String name : playerNames) {
 			this.players.add(new Player(name));
@@ -31,13 +44,11 @@ public class Game {
 		for (int i = 0; i < cards.length; i++) {
 			cards[i] = new Stack<LanternCard>();
 		}
-		// TODO - generate cards accordingly to players
 		
 		this.dedications = new Stack[DedicationType.values().length];
 		for (int i = 0; i < dedications.length; i++) {
 			dedications[i] = new Stack<DedicationToken>();
 		}
-		// TODO - generate dedications accordingly to players
 	}
 
 	public int getFavors() {
--- lanterns/src/main/java/ca/concordia/lanterns/services/impl/DefaultSetupService.java
+++ lanterns/src/main/java/ca/concordia/lanterns/services/impl/DefaultSetupService.java
@@ -1,51 +1,103 @@
 package ca.concordia.lanterns.services.impl;
 
+import java.util.Iterator;
+import java.util.Random;
 import java.util.Set;
 
 import ca.concordia.lanterns.entities.Game;
 import ca.concordia.lanterns.entities.Lake;
 import ca.concordia.lanterns.entities.LakeTile;
+import ca.concordia.lanterns.entities.Player;
+import ca.concordia.lanterns.entities.enums.Colour;
 import ca.concordia.lanterns.services.SetupService;
 
 public class DefaultSetupService implements SetupService {
 
 	public Game createGame(Set<String> playerNames) {
+		int playerCount = playerNames.size();
+		// TODO -  validate amount of players
 		Game game = new Game(playerNames);
-		LakeTile[] totalTiles = generateTiles(playerNames.size());
-				
+		LakeTile[] totalTiles = generateTiles(playerCount);
+		startLake(game.getLake(), totalTiles[0]);
+
 		// TODO - store game???
 		return game;
 	}
-	
+
+	/**
+	 * Generates tile stack depending on the number of players in the game.
+	 * 
+	 * @param playerCount
+	 *            number of players in the game.
+	 * @return Array of tiles.
+	 */
 	protected LakeTile[] generateTiles(int playerCount) {
 		LakeTile[] totalTiles = null;
-		
+
 		// TODO - maybe move quantity for each player count to properties/enum
 		switch (playerCount) {
 		case 4:
-			// 20 + 12 (3 tiles per player)
-			totalTiles = new LakeTile[32];
+			// 20 + 12 (3 tiles per player) + 1 initial tile
+			totalTiles = new LakeTile[33];
 			break;
 		case 3:
-			// 18 + 9 (3 tiles per player)
-			totalTiles = new LakeTile[27];
+			// 18 + 9 (3 tiles per player) + 1 initial tile
+			totalTiles = new LakeTile[28];
 			break;
 		case 2:
-			// 16 + 6 (3 tiles per player)
-			totalTiles = new LakeTile[22];
+			// 16 + 6 (3 tiles per player) + 1 initial tile
+			totalTiles = new LakeTile[23];
 			break;
 		default:
 			// TODO - throw exception
 			break;
 		}
+
+		// if total tiles is valid, populate it
 		if (totalTiles != null) {
-			// TODO - populate totalTiles
+			Random random = new Random();
+			Colour[] colours = Colour.values();
+			for (int i = 0; i < totalTiles.length; i++) {
+				Colour[] tileColours = new Colour[LakeTile.TOTAL_SIDES];
+				for (int j = 0; j < tileColours.length; j++) {
+					int nextColour = random.nextInt(colours.length);
+					tileColours[j] = colours[nextColour];
+				}
+				boolean platform = random.nextBoolean();
+				totalTiles[i] = new LakeTile(tileColours, platform);
+			}
 		}
-		
+
 		return totalTiles;
 	}
-	
-	protected void startLake(Lake lake, LakeTile initialTile) {
+
+	/**
+	 * Setup step #1:<br/>
+	 * Place the starting Lake Tile in the center of the play area face down. Flip the tile face up and orient it so that one
+	 * player is facing the red side and each other player is facing a different side.
+	 * 
+	 * @param lake
+	 * @param initialTile
+	 */
+	protected void startLake(final Lake lake, final LakeTile initialTile) {
 		lake.getTiles().add(initialTile);
+	}
+	
+	/**
+	 * Setup step #2:<br/>
+	 * Deal 3 Lake Tiles to each player face down. Lake Tiles are held in hand and kept secret from other players.
+	 */
+	protected void dealPlayerTiles(final LakeTile[] totalTiles, final Set<Player> players) {
+		final int playerCount = players.size();
+		int toAssign = (3*playerCount);
+		if (totalTiles.length > toAssign) {
+			while (toAssign > 0) {
+				for (Player player : players) {
+					player.getTiles().add(totalTiles[1+toAssign--]);
+				}
+			}
+		} else {
+			// FIXME - throw exception
+		}
 	}
 }
