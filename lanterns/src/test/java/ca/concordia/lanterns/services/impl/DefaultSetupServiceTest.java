ba1eb30ba5bbc05c2f4757b2759a4f95e83e8b7c
Gallindo, Gustavo <gustavo.gallindo@sap.com>
2015-07-10 03:20:00 -0400

    #2 - fixing import

--- lanterns/src/test/java/ca/concordia/lanterns/services/impl/DefaultSetupServiceTest.java
+++ lanterns/src/test/java/ca/concordia/lanterns/services/impl/DefaultSetupServiceTest.java
@@ -1,10 +1,15 @@
 package ca.concordia.lanterns.services.impl;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
