9878ac65c59e44419914ccd66fea089c3f9271ba
Parth Patel <shantmanoos@gmail.com>
2015-07-13 00:36:12 -0400

    Start Lake Tile can now be added to Lake

--- lanterns/src/main/java/ca/concordia/lanterns/entities/Lake.java
+++ lanterns/src/main/java/ca/concordia/lanterns/entities/Lake.java
@@ -2,6 +2,8 @@ package ca.concordia.lanterns.entities;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import ca.concordia.lanterns.entities.enums.PlayerID;
 /**  Lake entity.
  * <P> Various attributes of the lake and related behavior.
  * @version 1.0
@@ -16,7 +18,14 @@ public class Lake {
 	public Lake() {
 		super();
 		this.tiles = new ArrayList<LakeTile>();
+		
 	}
+	
+	public void placeStartTile ( LakeTile startTile, PlayerID[] orientation ) {
+		startTile.setOrientation(orientation);
+		this.tiles.add(startTile) ;
+	}
+	
 	/**
 	 * Returns the tiles that are part of the lake.
 	 * @return The lake tiles that are part of the lake.
