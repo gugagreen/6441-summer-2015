c921c9f2d1b47f6c3f42dcdbf29a33b87adb5b5e
ruixiangTan <love.yebuyu@gmail.com>
2015-07-14 16:48:31 -0400

    SetUp Menu State using Slick2D. If you hadn't set up the Slick2D library, please exclude the UI folder from your project. Will add more features later on.

---
+++ lanterns/src/main/java/ca/concordia/lanterns/UI/Menu.java
@@ -0,0 +1,153 @@
+package ca.concordia.lanterns.UI;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+/**
+ * Created by Ruixiang on 7/14/2015.
+ */
+public class Menu extends BasicGameState {
+
+    public Menu(int menu) {
+    }
+
+    @Override
+    public int getID() {
+        return 0;
+    }
+
+    @Override
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void enter(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public void leave(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public void controllerLeftPressed(int i) {
+
+    }
+
+    @Override
+    public void controllerLeftReleased(int i) {
+
+    }
+
+    @Override
+    public void controllerRightPressed(int i) {
+
+    }
+
+    @Override
+    public void controllerRightReleased(int i) {
+
+    }
+
+    @Override
+    public void controllerUpPressed(int i) {
+
+    }
+
+    @Override
+    public void controllerUpReleased(int i) {
+
+    }
+
+    @Override
+    public void controllerDownPressed(int i) {
+
+    }
+
+    @Override
+    public void controllerDownReleased(int i) {
+
+    }
+
+    @Override
+    public void controllerButtonPressed(int i, int i1) {
+
+    }
+
+    @Override
+    public void controllerButtonReleased(int i, int i1) {
+
+    }
+
+    @Override
+    public void keyPressed(int i, char c) {
+
+    }
+
+    @Override
+    public void keyReleased(int i, char c) {
+
+    }
+
+    @Override
+    public void mouseWheelMoved(int i) {
+
+    }
+
+    @Override
+    public void mouseClicked(int i, int i1, int i2, int i3) {
+
+    }
+
+    @Override
+    public void mousePressed(int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void mouseReleased(int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void mouseMoved(int i, int i1, int i2, int i3) {
+
+    }
+
+    @Override
+    public void mouseDragged(int i, int i1, int i2, int i3) {
+
+    }
+
+    @Override
+    public void setInput(Input input) {
+
+    }
+
+    @Override
+    public boolean isAcceptingInput() {
+        return false;
+    }
+
+    @Override
+    public void inputEnded() {
+
+    }
+
+    @Override
+    public void inputStarted() {
+
+    }
+}
