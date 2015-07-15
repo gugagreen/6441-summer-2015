cc11823d732edce0451b34109d582e2c755708a2
Armaan <Armaan@Gill>
2015-07-15 04:40:43 -0400

    Updating TestSuite

--- lanterns/src/test/java/ca/concordia/lanterns/TestSuite.java
+++ lanterns/src/test/java/ca/concordia/lanterns/TestSuite.java
@@ -3,13 +3,18 @@ package ca.concordia.lanterns;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import ca.concordia.lanterns.dao.impl.FileGameDaoTest;
 import ca.concordia.lanterns.entities.DedicationTokenTest;
 import ca.concordia.lanterns.entities.LakeTileTest;
+import ca.concordia.lanterns.entities.LanternCardTest;
+import ca.concordia.lanterns.entities.PlayerTest;
+import ca.concordia.lanterns.entities.TileSideTest;
 import ca.concordia.lanterns.readwrite.impl.JaxbGameMarshallerTest;
 import ca.concordia.lanterns.services.impl.DefaultSetupServiceTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({DedicationTokenTest.class, LakeTileTest.class, JaxbGameMarshallerTest.class, DefaultSetupServiceTest.class})
+@Suite.SuiteClasses({FileGameDaoTest.class,GameStubber.class,DedicationTokenTest.class, LakeTileTest.class, 
+	LanternCardTest.class, PlayerTest.class, TileSideTest.class , JaxbGameMarshallerTest.class, DefaultSetupServiceTest.class})
 public class TestSuite {
 	
 }
