1ac19d2306475658a5c3207bc4afba07457b3595
gladimus <lamothe.max@gmail.com>
2015-07-15 23:46:39 -0400

    Fixed comments

--- lanterns/src/main/java/ca/concordia/lanterns/services/SetupService.java
+++ lanterns/src/main/java/ca/concordia/lanterns/services/SetupService.java
@@ -24,8 +24,8 @@ public interface SetupService {
 	Game createGame(String[] playerNames);
 
 	/**
-	 * Setup step #1:<br/>
-	 * Place the starting Lake Tile in the center of the play area face down. Flip the tile face up and orient it so that one
+	 * Setup step #1:
+	 * <p>Place the starting Lake Tile in the center of the play area face down. Flip the tile face up and orient it so that one
 	 * player is facing the red side and each other player is facing a different side.
 	 * 
 	 * @param lake
@@ -34,8 +34,8 @@ public interface SetupService {
 	void startLake(final Lake lake, final LakeTile initialTile, int playerCount);
 
 	/**
-	 * Setup step #2:<br/>
-	 * Deal 3 Lake Tiles to each player face down. Lake Tiles are held in hand and kept secret from other players.
+	 * Setup step #2:
+	 * <p>Deal 3 Lake Tiles to each player face down. Lake Tiles are held in hand and kept secret from other players.
 	 * 
 	 * @param totalTiles
 	 * @param players
@@ -45,14 +45,14 @@ public interface SetupService {
 	void dealPlayerTiles(final LakeTile[] totalTiles, final Player[] players);
 
 	/**
-	 * Setup step #3:<br/>
-	 * Create a draw stack of Lake Tiles. The number of tiles in the stack depends on player count:<br/>
+	 * Setup step #3:
+	 * <p>Create a draw stack of Lake Tiles. The number of tiles in the stack depends on player count:
 	 * <ul>
 	 * <li>4 Players: 20 tiles</li>
 	 * <li>3 Players: 18 tiles</li>
 	 * <li>2 Players: 16 tiles</li>
 	 * </ul>
-	 * <br/>
+	 * 
 	 * By the end of this method, the tiles collection will be populated.
 	 * 
 	 * @param tiles
@@ -65,15 +65,16 @@ public interface SetupService {
 	void drawTileStack(final Stack<LakeTile> tiles, final LakeTile[] totalTiles, final int playerCount);
 
 	/**
-	 * Setup step #4:<br/>
-	 * Separate the Lantern Cards by color into 7 stacks. These stacks are collectively called the “supply.” The number of cards
-	 * in each stack depends on player count:<br/>
+	 * Setup step #4:
+	 * 
+	 * <p>Separate the Lantern Cards by color into 7 stacks. These stacks are collectively called the supply The number of cards
+	 * <p>in each stack depends on player count:
 	 * <ul>
 	 * <li>4 Players: 8 cards</li>
 	 * <li>3 Players: 7 cards</li>
 	 * <li>2 Players: 5 cards</li>
 	 * </ul>
-	 * <br/>
+	 *  
 	 * By the end of this method, the cards stacks will be populated.
 	 * 
 	 * @param cards
@@ -86,11 +87,11 @@ public interface SetupService {
 	void separateLanternCards(final LanternCardWrapper[] cards, final int playerCount);
 
 	/**
-	 * Setup step #5:<br/>
+	 * Setup step #5: 
 	 * Set aside the 3 generic Dedication Tokens.
-	 * <p/>
-	 * Setup step #6:<br/>
-	 * Separate Dedication Tokens by type into 3 stacks. Arrange each stack in descending order of value.<br/>
+	 * <p>
+	 * Setup step #6: 
+	 * Separate Dedication Tokens by type into 3 stacks. Arrange each stack in descending order of value. 
 	 * <ul>
 	 * <li>4 Players: use all tokens</li>
 	 * <li>3 Players: remove tokens with 4 dots</li>
@@ -109,16 +110,16 @@ public interface SetupService {
 	void setDedicationTokens(final DedicationTokenWrapper[] dedications, final int playerCount);
 
 	/**
-	 * Setup step #7:<br/>
+	 * Setup step #7: 
 	 * Give each player one Lantern Card corresponding to the color on the side of the starting Lake Tile he is facing. Each
-	 * player’s Lantern Cards are always kept in front of them, visible for everyone to see.
-	 * <p/>
+	 * players Lantern Cards are always kept in front of them, visible for everyone to see.
+	 * <p>
 	 * fyi, setup step #8 (Give the player with the red Lantern Card, the color of good fortune, the start player marker) is
 	 * unnecessary, as start player can be inferred by first tile in {@link Game#getLake()} plus first player in the {@link Game#getPlayers()}.
 	 * 
 	 * @param lake	game lake
 	 * @param cards	lantern cards available to be distributed
-	 * @param	the players in the game.
+	 * @param players the players in the game.
 	 */
 	void distributeInitialLanterns(final Lake lake, final LanternCardWrapper[] cards, final Player[] players);
 
