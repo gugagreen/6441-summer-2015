0bae53777bb8503381b699a5c13d89a2c6b71dd4
Gallindo, Gustavo <gustavo.gallindo@sap.com>
2015-07-20 18:36:51 -0400

    #28 - changing get to post on create game

--- lanterns/src/main/java/ca/concordia/lanterns/Controller/GameResource.java
+++ lanterns/src/main/java/ca/concordia/lanterns/Controller/GameResource.java
@@ -1,6 +1,6 @@
 package ca.concordia.lanterns.Controller;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
@@ -14,10 +14,9 @@ public class GameResource {
 
 	private SetupService setupService = DefaultSetupService.getInstance();
 
-	@GET
-	public Response getGame(@QueryParam("p1") final String p1, @QueryParam("p2") final String p2,
+	@POST
+	public Response createGame(@QueryParam("p1") final String p1, @QueryParam("p2") final String p2,
 			@QueryParam("p3") final String p3, @QueryParam("p4") final String p4) {
-		// FIXME - make it a POST instead of a GET, as it is creating a game
 		String[] playerNames = new String[] { p1, p2, p3, p4 };
 		Game game = setupService.createGame(playerNames);
 
