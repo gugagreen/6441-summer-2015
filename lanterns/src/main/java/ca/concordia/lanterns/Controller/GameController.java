7610f3e0ef7996319e189ee7b0b6e8d74cd140e7
gladimus <lamothe.max@gmail.com>
2015-07-29 17:36:16 -0400

    added loop to repeat a player turn if a tile is not properly placed
    Also changed how the lake is displayed slightly to make it easier to
    play.
    
    it appears that adjacent lookup is not being done properly. not sure if
    controller bug or method bug.

--- lanterns/src/main/java/ca/concordia/lanterns/Controller/GameController.java
+++ lanterns/src/main/java/ca/concordia/lanterns/Controller/GameController.java
@@ -27,20 +27,21 @@ import ca.concordia.lanternsentities.enums.DedicationType;
 public class GameController {
 	private Scanner keyboard;
 	private Game game;
-	
-//	private static final MarshallerManager<Game> marshaller = new JaxbGameMarshaller();
-	
+
+	// private static final MarshallerManager<Game> marshaller = new
+	// JaxbGameMarshaller();
+
 	public GameController() {
 		keyboard = new Scanner(System.in);
 	}
 
 	public static void displayCurrentGameState(Game game) {
-//		Writer writer = new StringWriter();
-//		marshaller.marshall(game, writer);
-//		System.out.println(writer.toString());
-		
-		//System.out.println(game);
-		for(int i=0; i< game.getPlayers().length; i++){
+		// Writer writer = new StringWriter();
+		// marshaller.marshall(game, writer);
+		// System.out.println(writer.toString());
+
+		// System.out.println(game);
+		for (int i = 0; i < game.getPlayers().length; i++) {
 			System.out.println("_______________________________");
 			System.out.println("Player " + game.getPlayers()[i].getId() + ": " + game.getPlayers()[i].getName());
 
@@ -48,71 +49,77 @@ public class GameController {
 			dispPlayerLanterns(game, i);
 			dispPlayerLakeTiles(game, i);
 			dispPlayerDedications(game, i);
-			
+			System.out.println("The lake currently has the following tiles in it:");
+			dispLake(game);
 		}
-		
+
 		System.out.println("_______________________________");
 	}
-	
-	private static void dispPlayerLanterns(Game game, final int playerID){
-		
-		for(int i =0; i < game.getCards().length ; i++){
+
+	private static void dispPlayerLanterns(Game game, final int playerID) {
+
+		for (int i = 0; i < game.getCards().length; i++) {
 			System.out.println("Lantern Card: " + game.getPlayers()[playerID].getCards()[i]);
 		}
 	}
-	
-	private static void dispPlayerLakeTiles(Game game, final int playerID){
-		
-		for(int i =0; i < game.getPlayers()[playerID].getTiles().size() ; i++){
-			
+
+	private static void dispPlayerLakeTiles(Game game, final int playerID) {
+
+		for (int i = 0; i < game.getPlayers()[playerID].getTiles().size(); i++) {
+
 			System.out.println("Lake Tile: " + i);
 			int cardSides = game.getPlayers()[playerID].getTiles().get(i).getSides().length;
-			for(int j = 0; j< cardSides ; j++){
+			for (int j = 0; j < cardSides; j++) {
 				System.out.println("	" + game.getPlayers()[playerID].getTiles().get(i).getSides()[j]);
 			}
-			
+
 			boolean tileHasPlatform = game.getPlayers()[playerID].getTiles().get(i).isPlatform();
-				if(tileHasPlatform){
-					System.out.println("	Lake Tile " + i + " has a platform");
-				}
-				else{
-					System.out.println("	Lake Tile " + i + " does not have a platform");
-				}
-			
-		}
-		
-	}
-	
-	private static void dispPlayerDedications(Game game, final int playerID){
-		
-		if(game.getPlayers()[playerID].getDedications().isEmpty()){
-			System.out.println(game.getPlayers()[playerID].getName() + " has no dedication tokens");
-		}
-		else{
-			
-				System.out.println("Dedications: " + game.getPlayers()[playerID].getDedications());
+			if (tileHasPlatform) {
+				System.out.println("	Lake Tile " + i + " has a platform");
+			} else {
+				System.out.println("	Lake Tile " + i + " does not have a platform");
+			}
 
 		}
-		
+
 	}
-	
+
+	private static void dispLake(Game game) {
+
+		for (int i = 0; i < game.getLake().size(); i++) {
+			System.out.println(i + "=" + game.getLake().get(i));
+		}
+	}
+
+	private static void dispPlayerDedications(Game game, final int playerID) {
+
+		if (game.getPlayers()[playerID].getDedications().isEmpty()) {
+			System.out.println(game.getPlayers()[playerID].getName() + " has no dedication tokens");
+		} else {
+
+			System.out.println("Dedications: " + game.getPlayers()[playerID].getDedications());
+
+		}
+
+	}
+
 	public void init() {
 		showMenu();
-		
+
 		while (true) {
-			int userChoice =  getValidInt("", 1, 3);
+			int userChoice = getValidInt("", 1, 3);
 			menuSelection(userChoice);
 		}
 	}
-	
+
 	private void showMenu() {
-		System.out.println("GROUP A SOEN 6441 LANTERNS BUILD 1");
+		System.out.println("GROUP A SOEN 6441 LANTERNS BUILD 2");
 		System.out.println("Select the following: ");
 		System.out.println("1) Start a new game");
 		System.out.println("2) Load game from file");
 		System.out.println("3) Quit");
 	}
-	
+
 	private void menuSelection(final int userChoice) {
 		switch (userChoice) {
 		case 1:
@@ -127,24 +134,25 @@ public class GameController {
 			System.out.println("Invalid Input, please try again.");
 		}
 	}
-	
+
 	private void startGame() {
 		int numberOfPlayers = getValidInt("Specify the number of players (2-4)", 2, 4);
 		String[] playerNames = new String[numberOfPlayers];
+		String[] hardcodeNames = new String[] { "Alpha", "Bravo", "Charlie", "Delta" };
 		for (int i = 0; i < numberOfPlayers; i++) {
-			playerNames[i] = Integer.toString(i);
+			playerNames[i] = hardcodeNames[i];
 		}
 
 		game = DefaultSetupService.getInstance().createGame(playerNames);
 		displayCurrentGameState(game);
 		System.out.println("Successfully Initialized game");
-		
+
 		gameSelection();
 	}
-	
+
 	private void gameSelection() {
 		int newGameOption = getValidInt("Select one option:\n1) Play\n2) Save Game\n3) Quit", 1, 3);
-		
+
 		switch (newGameOption) {
 		case 1:
 			if (game != null) {
@@ -170,7 +178,7 @@ public class GameController {
 			return;
 		}
 	}
-	
+
 	private void loadGame() {
 		String loadFileName = getValidString("Specify the name of the load file with the extension (.xml)");
 
@@ -179,19 +187,19 @@ public class GameController {
 
 		displayCurrentGameState(game);
 		System.out.println("Successfully loaded game");
-		
+
 		gameSelection();
 	}
-	
+
 	private void quit() {
 		System.out.println("So long, farewell, auf Wiedersehen, goodbye!");
 		keyboard.close();
 		System.exit(0);
 	}
-	
+
 	private void play() {
 		boolean isEnded = EndGameDetectService.getInstance().isGameEnded(game);
-		while(!isEnded) {
+		while (!isEnded) {
 			int currentIndex = game.getCurrentTurnPlayer();
 			Player currentPlayer = game.getPlayers()[currentIndex];
 			playTurn(currentPlayer);
@@ -205,34 +213,33 @@ public class GameController {
 			System.err.println(e.getMessage());
 		}
 	}
-	
+
 	private void playTurn(Player currentPlayer) {
 		System.out.println("_______________________________");
-		System.out.println("It is player '" + currentPlayer.getName() + "' turn.");
-		
+		System.out.println("It is player '" + currentPlayer.getName() + "'s turn.");
+
 		exchangeLantern(currentPlayer);
 		makeDedication(currentPlayer);
 		placeTile(currentPlayer);
-		
-		System.out.println("Player '" + currentPlayer.getName() + "' turn is finished.");
+
+		System.out.println("Player '" + currentPlayer.getName() + "'s turn is finished.");
 		System.out.println("Game state after this player turn:");
 		displayCurrentGameState(game);
 	}
-	
+
 	private void exchangeLantern(Player currentPlayer) {
 		System.out.println("You have [" + currentPlayer.getFavors() + "] favors.");
-		//System.out.println("Here are your lanterns:" + Arrays.toString(currentPlayer.getCards()));
-		
-		
+		// System.out.println("Here are your lanterns:" +
+		// Arrays.toString(currentPlayer.getCards()));
+
 		int doExchange = 2;
-		if(currentPlayer.getFavors() > 1){
+		if (currentPlayer.getFavors() > 1) {
 			dispPlayerLanterns(game, currentPlayer.getId());
 			doExchange = getValidInt("Do you want to make an exchange?\n1) Yes\n2) No", 1, 2);
-		}
-		else{
+		} else {
 			System.out.println("You do not have enough favour tokens to make an exchange this turn.");
 		}
-		
+
 		if (doExchange == 1) {
 			System.out.println("Select one card to give:");
 			int giveCardIndex = getValidInt(coloursWithIndexesString(), 0, 6);
@@ -241,19 +248,21 @@ public class GameController {
 			int receiveCardIndex = getValidInt(coloursWithIndexesString(), 0, 6);
 			Colour receiveCard = Colour.values()[receiveCardIndex];
 			try {
-				ActivePlayerService.getInstance().exchangeLanternCard(game, currentPlayer.getId(), giveCard, receiveCard);
+				ActivePlayerService.getInstance().exchangeLanternCard(game, currentPlayer.getId(), giveCard,
+						receiveCard);
 			} catch (GameRuleViolationException e) {
 				System.err.println(e.getMessage());
 			}
 		}
 	}
-	
-	private void makeDedication(Player currentPlayer)  {
-		//System.out.println("Here are your lanterns:" + Arrays.toString(currentPlayer.getCards()));
+
+	private void makeDedication(Player currentPlayer) {
+		// System.out.println("Here are your lanterns:" +
+		// Arrays.toString(currentPlayer.getCards()));
 		dispPlayerLanterns(game, currentPlayer.getId());
-		
+
 		int doDedication = getValidInt("Do you want to make a dedication?\n1) Yes\n2) No", 1, 2);
-		
+
 		if (doDedication == 1) {
 			System.out.println("Select one dedication type:");
 			int typeIndex = getValidInt(dedicationTypesString(), 0, 3);
@@ -272,35 +281,42 @@ public class GameController {
 			}
 		}
 	}
-	
-	private void placeTile(Player currentPlayer)  {
-		// active player must place a tile
-		System.out.println("Now it is time to place a tile.");
-		List<LakeTile> playerTiles = currentPlayer.getTiles();
-		dispPlayerLakeTiles(game, currentPlayer.getId());
-		int playerTileIndex = getValidInt("Select one of your tiles:", 0, playerTiles.size()-1);
 
-		
-		
-		System.out.println("Select one of the lake tiles to put your tile next to:");
-		List<LakeTile> lakeTiles = game.getLake();
-		int existingTileIndex = getValidInt(getTilesString(lakeTiles), 0, lakeTiles.size()-1);
-		
-		System.out.println("Select the side of the lake tiles to put your tile next to:");
-		LakeTile lakeTile = lakeTiles.get(existingTileIndex);
-		int existingTileSideIndex = getValidInt(getTileSidesString(lakeTile), 0, lakeTile.getSides().length-1);
-		
-		System.out.println("Select the side your tile to put next to the lake tile:");
-		LakeTile playerTile = playerTiles.get(playerTileIndex);
-		int playerTileSideIndex = getValidInt(getTileSidesString(playerTile), 0, playerTile.getSides().length-1);
-		
-		try {
-			ActivePlayerService.getInstance().placeLakeTile(game, currentPlayer.getId(), playerTileIndex, existingTileIndex, existingTileSideIndex, playerTileSideIndex);
-		} catch (GameRuleViolationException e) {
-			System.err.println(e.getMessage());
+	private void placeTile(Player currentPlayer) {
+		// active player must place a tile
+		Boolean playerNotDone = true;
+		while (playerNotDone) {
+			
+			System.out.println("Now it is time to place a tile.");
+			List<LakeTile> playerTiles = currentPlayer.getTiles();
+			dispPlayerLakeTiles(game, currentPlayer.getId());
+			int playerTileIndex = getValidInt("Select one of your tiles:", 0, playerTiles.size() - 1);
+
+			System.out.println("Select one of the lake tiles to put your tile next to:");
+			dispLake(game);
+			List<LakeTile> lakeTiles = game.getLake();
+			int existingTileIndex = getValidInt("", 0, lakeTiles.size() - 1);
+
+			System.out.println("Select the side of the lake tiles to put your tile next to:");
+			LakeTile lakeTile = lakeTiles.get(existingTileIndex);
+			int existingTileSideIndex = getValidInt(getTileSidesString(lakeTile), 0, lakeTile.getSides().length - 1);
+
+			System.out.println("Select the side your tile to put next to the lake tile:");
+			LakeTile playerTile = playerTiles.get(playerTileIndex);
+			int playerTileSideIndex = getValidInt(getTileSidesString(playerTile), 0, playerTile.getSides().length - 1);
+
+			try {
+				ActivePlayerService.getInstance().placeLakeTile(game, currentPlayer.getId(), playerTileIndex,
+						existingTileIndex, existingTileSideIndex, playerTileSideIndex);
+				playerNotDone = false;
+			} catch (GameRuleViolationException e) {
+				System.err.println(e.getMessage());
+				System.out.println();
+				System.out.println("Please reselect your tile and try again.");
+			}
 		}
 	}
-	
+
 	private void showWinner(Set<Player> winners) {
 		System.out.println("Game is ended!");
 
@@ -314,10 +330,10 @@ public class GameController {
 			for (LanternCardWrapper card : player.getCards()) {
 				sumCards += card.getQuantity();
 			}
-			System.out.println("Player [" + player.getName()+ "]:\tdedications=" + sumDedications 
-					+ "\tfavors=" + player.getFavors() + "\tlanters=" + sumCards);
+			System.out.println("Player [" + player.getName() + "]:\tdedications=" + sumDedications + "\tfavors="
+					+ player.getFavors() + "\tlanters=" + sumCards);
 		}
-		
+
 		if (winners.size() > 1) {
 			System.out.println("Game was a TIE!!!");
 		}
@@ -325,7 +341,7 @@ public class GameController {
 			System.out.println("Congratulations player [" + winner.getName() + "]! You are the winner!!!");
 		}
 	}
-	
+
 	private String coloursWithIndexesString() {
 		StringBuffer sb = new StringBuffer();
 		for (Colour colour : Colour.values()) {
@@ -333,7 +349,7 @@ public class GameController {
 		}
 		return sb.toString();
 	}
-	
+
 	private String dedicationTypesString() {
 		StringBuffer sb = new StringBuffer();
 		for (DedicationType type : DedicationType.values()) {
@@ -341,27 +357,27 @@ public class GameController {
 		}
 		return sb.toString();
 	}
-	
+
 	private String getTilesString(List<LakeTile> tiles) {
 		StringBuffer sb = new StringBuffer();
-		
+
 		for (int i = 0; i < tiles.size(); i++) {
 			sb.append(i + "=" + tiles.get(i) + "; ");
 		}
 		return sb.toString();
 	}
-	
+
 	private String getTileSidesString(LakeTile tile) {
 		StringBuffer sb = new StringBuffer();
-		
+
 		TileSide[] sides = tile.getSides();
-		
+
 		for (int i = 0; i < sides.length; i++) {
 			sb.append(i + "=" + sides[i] + "; ");
 		}
 		return sb.toString();
 	}
-	
+
 	private int getValidInt(final String message, final int min, final int max) {
 		System.out.print(message);
 		int userChoice = 0;
@@ -382,14 +398,14 @@ public class GameController {
 		System.out.println(userChoice);
 		return userChoice;
 	}
-	
+
 	private String getValidString(final String message) {
 		System.out.print(message);
 		String value = null;
 		boolean valid = false;
 		while (!valid) {
 			value = keyboard.next();
-			if(value != null && value.length() > 0) {
+			if (value != null && value.length() > 0) {
 				valid = true;
 			} else {
 				System.out.println("Invalid input. Please enter a valid String: ");
