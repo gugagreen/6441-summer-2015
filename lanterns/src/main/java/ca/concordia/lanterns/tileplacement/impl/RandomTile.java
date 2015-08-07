012d8d6854384559ae3a03cfc153d5ed7a729a38
gladimus <lamothe.max@gmail.com>
2015-08-07 15:05:07 -0400

    Formatting fix

--- lanterns/src/main/java/ca/concordia/lanterns/tileplacement/impl/RandomTile.java
+++ lanterns/src/main/java/ca/concordia/lanterns/tileplacement/impl/RandomTile.java
@@ -1,6 +1,5 @@
 package ca.concordia.lanterns.tileplacement.impl;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -17,40 +16,38 @@ public class RandomTile implements TilePlayBehavior {
 
 	@Override
 	public void placeTile(Game game, Player currentPlayer) throws GameRuleViolationException {
-		
 
-		//choosing a random player tile
-        int playerTileIndex = getRandPlayerTile(currentPlayer);
-        
-        //Select the side of the tile to put next to the lake tile:");
-        int playerTileSideIndex = getRandTileSideIndex();
-        
-        //Select one of the lake tiles to put your tile next to:");
+		//making sure that player is not trying to place tiles without tiles
+		if (!currentPlayer.getTiles().isEmpty()) {
+			// choosing a random player tile
+			int playerTileIndex = getRandPlayerTile(currentPlayer);
 
-        LakeTile lakeTile = randLakeTile(game);
-        List<Integer> usableTileSides = usableLakeTileSides(lakeTile);
-        int existingTileSideIndex = randLakeTileSide(usableTileSides);
+			// Select the side of the tile to put next to the lake tile:");
+			int playerTileSideIndex = getRandTileSideIndex();
 
-        ActivePlayerService.getInstance().placeLakeTile(game, currentPlayer.getId(), playerTileIndex,	lakeTile.getId(), existingTileSideIndex, playerTileSideIndex);
-			
+			// Select one of the lake tiles to put your tile next to:");
+
+			LakeTile lakeTile = randLakeTile(game);
+			List<Integer> usableTileSides = usableLakeTileSides(lakeTile);
+			int existingTileSideIndex = randLakeTileSide(usableTileSides);
+
+			ActivePlayerService.getInstance().placeLakeTile(game, currentPlayer.getId(), playerTileIndex,
+					lakeTile.getId(), existingTileSideIndex, playerTileSideIndex);
+		}
 	}
-	
-	
 
-	
 	private int randLakeTileSide(List<Integer> usableTileSides) {
 		Random rand = new Random();
 		int randomSideIndex = rand.nextInt(usableTileSides.size());
+		int randUsableTileSide = usableTileSides.get(randomSideIndex);
 		
-		return usableTileSides.get(randomSideIndex);
+		return randUsableTileSide;
 	}
 
-
-
-	private List<String> possibleLakeTile(Game game){
+	private List<String> possibleLakeTile(Game game) {
 		List<String> possibleLakeTiles = new ArrayList<String>();
 		LakeTile[][] matrix = game.getLake();
-		
+
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
 				if (matrix[i][j] != null) {
@@ -60,15 +57,15 @@ public class RandomTile implements TilePlayBehavior {
 		}
 		return possibleLakeTiles;
 	}
-	
-	private List<String> usableLakeTilesIds(Game game){
+
+	private List<String> usableLakeTilesIds(Game game) {
 		List<String> possibleLakeTiles = possibleLakeTile(game);
 		List<String> usableLakeTiles = new ArrayList<String>();
-		
-		for(String lakeTile: possibleLakeTiles){
-			for(int i = 0; i<4; i++){
-				boolean validSide = validLakeTileSide(MatrixOrganizer.findTile(game.getLake(), lakeTile),i);
-				if(validSide){
+
+		for (String lakeTile : possibleLakeTiles) {
+			for (int i = 0; i < 4; i++) {
+				boolean validSide = validLakeTileSide(MatrixOrganizer.findTile(game.getLake(), lakeTile), i);
+				if (validSide) {
 					usableLakeTiles.add(lakeTile);
 					break;
 				}
@@ -76,53 +73,50 @@ public class RandomTile implements TilePlayBehavior {
 		}
 		return usableLakeTiles;
 	}
-	
-	private LakeTile randLakeTile(Game game){
+
+	private LakeTile randLakeTile(Game game) {
 		Random rand = new Random();
 		List<String> usableLakeTiles = usableLakeTilesIds(game);
 		int randUsableLakeTileIndex = rand.nextInt(usableLakeTiles.size());
-	
+
 		String lakeTileId = usableLakeTiles.get(randUsableLakeTileIndex);
 		return MatrixOrganizer.findTile(game.getLake(), lakeTileId);
-	
+
 	}
 
-	private int getRandPlayerTile(Player currentPlayer){
+	private int getRandPlayerTile(Player currentPlayer) {
 		Random rand = new Random();
 		int lakeTiles = currentPlayer.getTiles().size();
 		int randTile = rand.nextInt(lakeTiles);
-		
+
 		return randTile;
 	}
-	
-	private int getRandTileSideIndex(){
+
+	private int getRandTileSideIndex() {
 		Random rand = new Random();
 		int tileSide = rand.nextInt(4);
-		
+
 		return tileSide;
 	}
-	
-	private boolean validLakeTileSide(LakeTile lakeTile, int checkSide){
-		
-		if(lakeTile.getSides()[checkSide].getAdjacent() == null){
+
+	private boolean validLakeTileSide(LakeTile lakeTile, int checkSide) {
+
+		if (lakeTile.getSides()[checkSide].getAdjacent() == null) {
 			return true;
-		}
-		else{
+		} else {
 			return false;
 		}
 	}
-	
-	private List<Integer> usableLakeTileSides(LakeTile lakeTile){
+
+	private List<Integer> usableLakeTileSides(LakeTile lakeTile) {
 		List<Integer> usableTileSides = new ArrayList<Integer>();
-		
+
 		for (int i = 0; i < 4; i++) {
-			if(validLakeTileSide(lakeTile, i)){
+			if (validLakeTileSide(lakeTile, i)) {
 				usableTileSides.add(i);
 			}
 		}
 		return usableTileSides;
 	}
-	
-	
-}
 
+}
