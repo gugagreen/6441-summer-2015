aadaed7992bf09548cf429100b8ed46b6af2ecdc
gladimus <lamothe.max@gmail.com>
2015-08-07 14:14:04 -0400

    Fixed bugs in RandomExchange

--- lanterns/src/main/java/ca/concordia/lanterns/exchange/impl/RandomExchange.java
+++ lanterns/src/main/java/ca/concordia/lanterns/exchange/impl/RandomExchange.java
@@ -1,5 +1,7 @@
 package ca.concordia.lanterns.exchange.impl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import ca.concordia.lanterns.exception.GameRuleViolationException;
@@ -18,10 +20,10 @@ public class RandomExchange implements ExchangeBehavior {
 		if(allowExchange()){
 		
 			//Select one card to give
-			int giveCardIndex = randomLanternIndex();
+			int giveCardIndex = randomLanternIndex(possibleLanternIndex(game, currentPlayer));
 			Colour giveCard = Colour.values()[giveCardIndex];
 			//Select one card to receive
-			int receiveCardIndex = otherRandLanternIndex(giveCardIndex);
+			int receiveCardIndex = randIndexWithExclusion(giveCardIndex, possibleTableLanternsIndex(game) );
 			Colour receiveCard = Colour.values()[receiveCardIndex];
 					
 			ActivePlayerService.getInstance().exchangeLanternCard(game, currentPlayer.getId(), giveCard, receiveCard);
@@ -36,16 +38,38 @@ public class RandomExchange implements ExchangeBehavior {
 		return makeExchange;	
 	}
 	
-	private static int randomLanternIndex(){
+	private static int randomLanternIndex(List<Integer> allowablePlayerLanterns){
 		Random rand = new Random();
-		int randLantern = rand.nextInt(6);
+		int randLantern = rand.nextInt(allowablePlayerLanterns.size());
 		
-		return randLantern;
+		return allowablePlayerLanterns.get(randLantern) ;
 	}
 	
-	private int otherRandLanternIndex(int excludedIndex){
+	private List<Integer> possibleLanternIndex(Game game, Player currentPlayer){
+		List<Integer> allowPlayerLanterns = new ArrayList<Integer>(); 
+		
+		for (int i=0; i<7;i++) {
+			if(currentPlayer.getCards()[i].getQuantity()>0){
+				allowPlayerLanterns.add(i);
+			}
+		}
+		return allowPlayerLanterns;
+	}
+	
+	private List<Integer> possibleTableLanternsIndex(Game game){
+		List<Integer> allowTableLanterns = new ArrayList<Integer>(); 
+		
+		for (int i=0; i<7;i++) {
+			if(game.getCards()[i].getQuantity()>0){
+				allowTableLanterns.add(i);
+			}
+		}
+		return allowTableLanterns;
+	}
+	
+	private int randIndexWithExclusion(int excludedIndex, List<Integer> possibleTableLanterns){
 		boolean searchingRand = true;
-		int newIndex = randomLanternIndex();	
+		int newIndex = randomLanternIndex(possibleTableLanterns);	
 		
 		while(searchingRand){
 			
@@ -53,7 +77,7 @@ public class RandomExchange implements ExchangeBehavior {
 				searchingRand = false;
 			}
 			else if(newIndex == excludedIndex){
-				newIndex = randomLanternIndex();
+				newIndex = randomLanternIndex(possibleTableLanterns);
 			}
 		}
 		return newIndex;	
