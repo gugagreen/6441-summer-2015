afd151902f3569ade2168587365bf45e970c2367
Parth Patel <shantmanoos@gmail.com>
2015-08-11 16:50:25 -0400

    #43 Intial part for unfriendlyAI

--- lanterns/src/main/java/ca/concordia/lanterns/exchange/impl/WorstExchange.java
+++ lanterns/src/main/java/ca/concordia/lanterns/exchange/impl/WorstExchange.java
@@ -1,8 +1,13 @@
 package ca.concordia.lanterns.exchange.impl;
 
+import java.util.Arrays;
+
+import ca.concordia.lanternsentities.DedicationToken;
+import ca.concordia.lanternsentities.DedicationTokenWrapper;
 import ca.concordia.lanternsentities.Game;
 import ca.concordia.lanternsentities.Player;
 import ca.concordia.lanternsentities.ai.ExchangeBehavior;
+import ca.concordia.lanternsentities.enums.DedicationType;
 
 public class WorstExchange implements ExchangeBehavior{
 
@@ -10,5 +15,55 @@ public class WorstExchange implements ExchangeBehavior{
 	public void makeExchange(Game game, Player currentPlayer) {
 		// TODO Auto-generated method stub
 		
+		DedicationType[] sortedGameDedications = sortDedications (game.getDedications());
+		
+		int nextPlayerIndex = game.getCurrentTurnPlayer() + 1;
+		Player[] players = game.getPlayers();
+		
+		int i = 0 ;
+		
+		while(i != players.length - 1){
+			nextPlayerIndex = nextPlayerIndex % 4 ;
+			
+			for (DedicationType dedicationType: sortedGameDedications){
+				if (isDedicationThreat(dedicationType, players[i])){
+					
+				}
+			}
+			
+			++nextPlayerIndex;
+			++i;
+		}
+	}
+	
+	private boolean isDedicationThreat(DedicationType dedicationType, Player player){
+		
+	}
+	
+	private DedicationType[] sortDedications(DedicationTokenWrapper[] gameDedications){
+		int[] sortedGameDedications = new int[gameDedications.length - 1];
+		DedicationType[] sortedType = new DedicationType[gameDedications.length - 1];
+		
+		int key;
+		int j;
+		sortedGameDedications[0] = gameDedications[0].getStack().peek().getTokenValue();
+		sortedType[0] = gameDedications[0].getStack().peek().getTokenType();
+		
+		for (int i = 1; i != gameDedications.length - 1; ++i){
+			DedicationToken top = gameDedications[i].getStack().peek();
+				key = top.getTokenValue();
+				
+				j = i - 1;
+				while (j != -1 && sortedGameDedications[j] < key){
+					sortedGameDedications[j+1] = sortedGameDedications[j];
+					sortedType[j+1] = sortedType[j];
+					--j ;
+				}
+				sortedGameDedications[j+1] = key;
+				sortedType[j+1] = top.getTokenType();
+		}
+		
+		
+		return sortedType;	
 	}
 }
---
+++ lanterns/src/main/java/ca/concordia/lanterns/exchange/impl/helper/DedicationThreat.java
@@ -0,0 +1,80 @@
+package ca.concordia.lanterns.exchange.impl.helper;
+
+import java.util.Arrays;
+import java.util.List;
+
+import ca.concordia.lanterns.services.enums.DedicationCost;
+import ca.concordia.lanternsentities.DedicationToken;
+import ca.concordia.lanternsentities.Game;
+import ca.concordia.lanternsentities.LanternCardWrapper;
+import ca.concordia.lanternsentities.Player;
+import ca.concordia.lanternsentities.enums.Colour;
+import ca.concordia.lanternsentities.enums.DedicationType;
+
+public class DedicationThreat {
+	
+	private static final List<DedicationType> dedicationType = Arrays.asList(DedicationType.values());
+	
+	// The token that will be earned from the game if the threat is realised.
+	private DedicationToken possibleEarning;
+	
+	// The color that is absent in order to make the dedication
+	private Colour absentColour;
+	
+	// The game in context of which their is a threat
+	private Game game;
+	
+	private DedicationThreat(DedicationToken posssibleEarning, Colour absentColour, Game game){
+		this.possibleEarning = possibleEarning;
+		this.absentColour = absentColour;
+		this.game = game;
+	}
+	
+	public static DedicationThreat getThreat(DedicationType dedicationType, Player player, Game game){
+		DedicationThreat threat = null;
+		
+		if (dedicationType == DedicationType.FOUR_OF_A_KIND){
+			threat = getFourOfAKind(player, game);
+		} else if(dedicationType == DedicationType.THREE_PAIRS){
+			threat = getThreePairs(player, game);
+		} else if(dedicationType == DedicationType.SEVEN_UNIQUE){
+			threat = getSevenUnique(player, game);
+		}
+		
+		return threat;
+	}
+	
+	private static DedicationThreat getFourOfAkind(Player player, Game game){
+		DedicationThreat threat = null;
+		
+		LanternCardWrapper[]playerCards = game.getCards();
+		LanternCardWrapper[] gameCards = game.getCards();
+		
+		int minGameCardQuantity = -1;
+		Colour absentColour = null;
+		for (int i = 0; i != playerCards.length; ++i){
+			int gameCardQuantity = gameCards[i].getQuantity();
+			
+			if (playerCards[i].getQuantity() == DedicationCost.FOUR_OF_A_KIND.getRequiredCardPerColor() - 1 && gameCardQuantity >= 1){
+				
+				if (minGameCardQuantity == -1){
+					absentColour = playerCards[i].getColour();
+					minGameCardQuantity = gameCardQuantity;
+				} else if (gameCards[i].getQuantity() < minGameCardQuantity){
+						absentColour = playerCards[i].getColour();
+						minGameCardQuantity = gameCards[i].getQuantity();
+				}
+			}
+		}
+		
+		if (absentColour != null){
+			int tokenValue = game.getDedications()[dedicationType.indexOf(DedicationType.FOUR_OF_A_KIND)].getStack().peek().getTokenValue();
+			
+			DedicationToken possibleEarning = new DedicationToken();
+			possibleEarning.init(tokenValue, DedicationType.FOUR_OF_A_KIND);
+			threat = new DedicationThreat(new DedicationToken(), absentColour, game);
+		}
+		
+		return threat;
+	}
+}
