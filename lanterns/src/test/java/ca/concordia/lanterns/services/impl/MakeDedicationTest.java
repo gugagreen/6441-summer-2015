d5b7bbb8da66626d572c6bdbc63533836851216a
Armaan <Armaan@Gill.wireless.concordia.ca>
2015-08-12 15:40:46 -0400

    Enhancing test cases

--- lanterns/src/test/java/ca/concordia/lanterns/services/impl/MakeDedicationTest.java
+++ lanterns/src/test/java/ca/concordia/lanterns/services/impl/MakeDedicationTest.java
@@ -141,6 +141,7 @@ public class MakeDedicationTest {
     public void insufficientPlayerLanternCard() {
         Player firstPlayer = game.getPlayers()[0];
         LanternCardWrapper[] card = firstPlayer.getCards();
+        assertNotNull(card);
         card[0].setQuantity(3);
         card[2].setQuantity(1);
         card[6].setQuantity(0);
@@ -212,7 +213,7 @@ public class MakeDedicationTest {
             }
 
             DedicationToken currentTopToken = game.getDedications()[k].getStack().peek();
-
+            assertNotNull(currentTopToken);
             assertSame(lastReceivedToken, currentLastToken);
             assertSame(topToken, currentTopToken);
 
